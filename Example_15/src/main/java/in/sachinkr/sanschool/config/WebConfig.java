5f52eeee0ab5db0350b4d57e0ae5d790d986e359
Sachin Kumar <sachinyadav1469@gmail.com>
2023-03-17 23:55:32 +0530

    School Website project frontend setup

---
+++ Example_15/src/main/java/in/sachinkr/sanschool/config/WebConfig.java
@@ -0,0 +1,18 @@
+package in.sachinkr.sanschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/contact").setViewName("contact");
+        registry.addViewController("/courses").setViewName("courses");
+        registry.addViewController("/about").setViewName("about");
+
+    }
+}
