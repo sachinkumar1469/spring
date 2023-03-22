e4b062b4dc9d3c8145446998d4f329b07e8c409f
Sachin Kumar <sachinyadav1469@gmail.com>
2023-03-22 23:53:22 +0530

    Login, Logout, Roles and inMemoryAuthentication

---
+++ Example_22/src/main/java/in/sachinkr/sanschool/model/Holiday.java
@@ -0,0 +1,15 @@
+package in.sachinkr.sanschool.model;
+
+import lombok.Data;
+
+@Data
+public class Holiday {
+    private final String day;
+    private final String reason;
+    private final Type type;
+
+    public enum Type {
+        FESTIVAL, FEDERAL
+    }
+
+}
---
+++ Example_22/src/test/java/in/sachinkr/sanschool/SanschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sachinkr.sanschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
